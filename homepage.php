960eff5a9a73de31b832409d21b79bf1ad256811
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-15 09:58:58 +0800

    homepage structure

--- homepage.php
+++ homepage.php
@@ -1,3 +1,100 @@
-<?php
-echo "hey";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial;}
+
+/* Style the tab */
+.tab {
+  overflow: hidden;
+  border: 1px solid #ccc;
+  background-color: #f1f1f1;
+}
+
+/* Style the buttons inside the tab */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+  font-size: 17px;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
+}
+</style>
+</head>
+<body>
+
+
+<div class="tab">
+  <button class="tablinks" onclick="openCity(event, 'Home')">Home</button>
+  <button class="tablinks" onclick="openCity(event, 'Learn')">Learn</button>
+</div>
+
+<div id="Home" class="tabcontent">
+  <h3>Courses</h3>
+  <a href='python.html'>
+    <button>Python</button>
+  </a>
+  <p></p>
+  <a href='C++.html'>
+    <button>C++</button>
+  </a>
+  <p></p>
+  <a href='HTML.html'>
+    <button>HTML</button>
+  </a>
+  <p></p>
+  <a href='scratch.html'>
+    <button>Scratch</button>
+  </a>
+</div>
+
+<div id="Learn" class="tabcontent">
+  <h3>Learn</h3>
+  <p>Courses you have signed up for</p> 
+  <!-- 
+if not signed up for any, print oops not signed up for anything
+else, display the button + picture
+      !-->
+</div>
+
+
+<script>
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+</script>
+   
+</body>
+</html> 
