fd122b33d437cffd7815d2073cc58b08371ea33f
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-30 18:40:18 +0800

    python workspace setup

---
+++ python/partOneRead.php
@@ -0,0 +1,12 @@
+<?php
+echo "One of the most important things you need to be able to do is to display a message.";
+echo "<br>";
+echo "In python, this is called printing."
+echo "<br>";
+echo "To do so, you would write -    print()"
+echo "<br>";
+echo "For example, if you wanted to print out Hello World, you would write";
+echo "<br>";
+echo "print('Hello World')";
+
+?>
---
+++ python/pythonSort.php
@@ -0,0 +1,4 @@
+<?php
+//get level
+//take according to level 
+?>
