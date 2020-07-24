1e23de68e4729f4570667a2c77c188a0a5a9df8d
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-07-24 16:58:24 +0800

    Update pageFour.php

--- python/indiv/pageFour.php
+++ python/indiv/pageFour.php
@@ -1,1 +1,157 @@
-
+<html>
+<head>
+</head>
+<body>
+<style>
+body {
+  background: #FFF;
+  padding-top: 10px;
+}
+p {
+  color: red;
+  font-family: "Courier";
+  font-size: 20px;
+  opacity: 0;
+  animation: type 90s steps(60, end) forwards;
+  -webkit-user-select: none;
+  user-select: none;
+}
+p:nth-child(2) {
+  animation-delay: 0.5s;
+}
+p:nth-child(3) {
+  animation-delay: 1.5s;
+}
+p:nth-child(4) {
+  animation-delay: 2.5s;
+}
+p:nth-child(5) {
+  animation-delay: 3.5s;
+}
+p:nth-child(6) {
+  animation-delay: 4.5s;
+  margin-bottom: 25px;
+}
+p:nth-child(7) {
+  animation-delay: 5.5s;
+}
+p:nth-child(8) {
+  animation-delay: 6.5s;
+}
+p:nth-child(9) {
+  animation-delay: 7.5s;
+}
+p:nth-child(10) {
+  animation-delay: 8.5s;
+}
+p:nth-child(11) {
+  animation-delay: 9.5s;
+}
+p:nth-child(12) {
+  animation-delay: 10.5s;
+}
+p:nth-child(13) {
+  animation-delay: 11.5s;
+}
+p:nth-child(14) {
+  animation-delay: 12.5s;
+}
+p:nth-child(15) {
+  animation-delay: 13.5s;
+}
+p:nth-child(16) {
+  animation-delay: 14.5s;
+}
+p:nth-child(17) {
+  animation-delay: 15.5s;
+}
+p:nth-child(18) {
+  animation-delay: 16.5s;
+}
+p:nth-child(19) {
+  animation-delay:17.5s;
+}
+p:nth-child(20) {
+  animation-delay: 18.5s;
+}
+p:nth-child(21) {
+  animation-delay: 19.5s;
+}
+p:nth-child(22) {
+  animation-delay: 20.5s;
+}
+p:nth-child(23) {
+  animation-delay: 21.5s;
+}
+p:nth-child(24) {
+  animation-delay: 22.5s;
+}
+p:nth-child(25) {
+  animation-delay: 23.5s;
+}
+p:nth-child(26) {
+  animation-delay: 24.5s;
+}
+p:nth-child(27) {
+  animation-delay: 25.5s;
+}
+p:nth-child(28) {
+  animation-delay: 26.5s;
+}
+p:nth-child(29) {
+  animation-delay: 27.5s;
+}
+p:nth-child(30) {
+  animation-delay: 28.5s;
+}
+@keyframes type {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    width: 30em;
+    opacity: 1;
+  }
+}
+@keyframes blink {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+::selection {
+  background: black;
+}
+</style>
+<p>ARITHMETIC CALCULATIONS</p>
+<p>A key and important skill is being able to do arithmetics in programming</p>
+<p>There are 4 types - addition, subtraction, multiplication and divisijon</p>
+<p>Addition is adding two numbers, to do so you would write: sum = num1 + num2, where it would add num1 and num2 up.</p>
+<p>The complete code would be:</p>
+<p>num1=input()</p>
+<p>num2=input()</p>
+<p>sum=num1+num2</p>
+<p>print(sum)</p>
+<p>Subtraction is subtracting two numbers, to do so you would write: subtract = num1 - num2, where it would subtract num1 and num2.</p>
+<p>The complete code would be:</p>
+<p>num1=input()</p>
+<p>num2=input()</p>
+<p>subtract = num1 - num2</p>
+<p>print(subtract)</p>
+<p>Multiplication is multplying two numbers, to do so you would write: result = num1 * num2, where it would multiply num1 and num2.</p>
+<p>The complete code would be:</p>
+<p>num1=input()</p>
+<p>num2=input()</p>
+<p>result = num1 * num22</p>
+<p>print(result)</p>
+<p>Division is dividing two numbers, to do so you would write: result = num1 / num2, where it would div ide num1 by num2.</p>
+<p>The complete code would be:</p>
+<p>num1=input()</p>
+<p>num2=input()</p>
+<p>result = num1 / num22</p>
+<p>print(result)</p>
+<p>click <a href="partFourQuestionOne.php"> here</a> to go to the next page</p>
+</body>
+</html>
