4ddc10d28941272e8c2656fb677b9c786b687c9e
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-07-26 19:14:27 +0800

    Update partFourQuestionThree.php

--- python/indiv/partFourQuestionThree.php
+++ python/indiv/partFourQuestionThree.php
@@ -1,22 +1,53 @@
 <!DOCTYPE html> 
-<html>   
-<head> 
-</head>   
-<body>        
-    <h1 style="color:green;"> Question Three</h1> 
-    <h3>Which of the following would print the result of 5+5/5*55?</h3>  
-    <form> 
-        <input type="radio" name="Hello" id="w1"
+<html> 
+<title>TeachMe</title>
+<head>
+<link rel="shortcut icon" href="logo.JPG" type="image/x-icon">
+</head>
+  
+<body>  
+<style>
+div.a {
+  border: 1px solid black;
+}
+div.outer { position: relative; height: 2px; }
+div.outer img { position: fixed; right: 0; bottom: 0; }
+</style>
+<br>
+<br>
+<br>
+<br>
+<div class="a" style="max-width:800px;margin:auto;vertical-align: middle;padding:10px;">
+    <h1 style="color:green;font-family: 'Bubblegum Sans', cursive; font-size: 30px;"> 
+    Question Three
+    </h1> 
+      
+    <h3 style="font-family: 'Bubblegum Sans', cursive;font-size: 25px;"> 
+    Which of the following would print the result of 5+5/5*55?
+    </h3> 
+      
+    <form style="font-family: 'Bubblegum Sans', cursive;font-size: 25px;"> 
+    <input type="radio" name="Hello" id="w1"
             value="c1">print(5+5/5*5)<br> 
+          
         <input type="radio" name="Hello" id="c1"
             value="c1">print(5+5/5*55)<br> 
+          
         <input type="radio" name="Hello" id="w2"
             value="w2">result=5+5/5<br><br> 
-        <button type="button" onclick="display()"> Submit </button> 
+      
+        <button type="button" onclick="display()"> 
+            Submit 
+        </button> 
     </form> 
     <br> 
-    <div id="disp" style= "color:green; font-size:18px; font-weight:bold;"> 
+    <div id="disp" style= 
+        "color:green; font-size:18px; font-weight:bold;"> 
     </div>  
+</div>
+<div class="outer">
+    <img src="coding.gif" height="100">
+ </div>
 </body>  
     <script> 
         function display() {  
@@ -24,19 +55,23 @@
                 var partOne = "Good try but this is incorrect. Remember to check what calculation needs to be done.";
                     var str = "Next Question";
                     var result = str.link("finished.php");
-                    document.getElementById("disp").innerHTML = partOne  + "      " + result;
+                    var total = partOne  + "      " + result;
+                    document.getElementById("disp").innerHTML = total.fontcolor("red")
+                
             } 
             else if(document.getElementById('w2').checked) { 
                var partOne = "Not Quite! Remember,the task is to print out the result, not store the result.";  
                var str = "Next Question";
                     var result = str.link("finished.php");
-                    document.getElementById("disp").innerHTML = partOne  + "      " + result; 
+                    var total = partOne  + "      " + result;
+                    document.getElementById("disp").innerHTML = total.fontcolor("red")
             } 
             else if(document.getElementById('c1').checked) { 
                 var partOne = "This is correct!";   
                 var str = "Next Question";
                     var result = str.link("finished.php");
-                    document.getElementById("disp").innerHTML = partOne  + "      " + result;
+                    var total = partOne  + "      " + result;
+                    document.getElementById("disp").innerHTML = total.fontcolor("green")
             } 
             else { 
                 var partOne = "The task was to print the result of the calculation of 5+5/5*55, so the second option was the correct answer - print(5+5/5*55)"; 
@@ -46,4 +81,4 @@
             } 
         } 
     </script> 
-</html> 
+</html>
