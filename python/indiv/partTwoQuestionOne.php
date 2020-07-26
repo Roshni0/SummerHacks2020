fa430d492c7db2e7dde5402acef902a00e1884b5
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-07-26 19:05:14 +0800

    Update partTwoQuestionOne.php

--- python/indiv/partTwoQuestionOne.php
+++ python/indiv/partTwoQuestionOne.php
@@ -1,25 +1,53 @@
 <!DOCTYPE html> 
 <html> 
-<head> 
-</head> 
+<title>TeachMe</title>
+<head>
+<link rel="shortcut icon" href="logo.JPG" type="image/x-icon">
+</head>
+  
 <body>  
-    <h1 style="color:green;">Question One</h1> 
-    <h3>Which of the following would store the word yellow in the variable 'colour'?</h3>   
-    <form> 
-        <input type="radio" name="Hello" id="w1"
+<style>
+div.a {
+  border: 1px solid black;
+}
+div.outer { position: relative; height: 2px; }
+div.outer img { position: fixed; right: 0; bottom: 0; }
+</style>
+<br>
+<br>
+<br>
+<br>
+<div class="a" style="max-width:800px;margin:auto;vertical-align: middle;padding:10px;">
+    <h1 style="color:green;font-family: 'Bubblegum Sans', cursive; font-size: 30px;"> 
+    Question One
+    </h1> 
+      
+    <h3 style="font-family: 'Bubblegum Sans', cursive;font-size: 25px;"> 
+    Which of the following would store the word yellow in the variable 'colour'?
+    </h3> 
+      
+    <form style="font-family: 'Bubblegum Sans', cursive;font-size: 25px;"> 
+    <input type="radio" name="Hello" id="w1"
             value="w1">colour=yellow<br> 
+          
         <input type="radio" name="Hello" id="c1"
             value="c1">colour = "yellow"<br> 
+          
         <input type="radio" name="Hello" id="w2"
             value="w2">color = "yellow"<br><br> 
+      
         <button type="button" onclick="display()"> 
             Submit 
-        </button> 
+        </button>  
     </form> 
     <br> 
     <div id="disp" style= 
         "color:green; font-size:18px; font-weight:bold;"> 
     </div>  
+</div>
+<div class="outer">
+    <img src="coding.gif" height="100">
+ </div>
 </body>  
     <script> 
         function display() {  
@@ -27,20 +55,23 @@
                 var partOne = "Good try but this is incorrect. Remember, you need to have quotation marks around the text.";
                     var str = "Next Question";
                     var result = str.link("partTwoQuestionTwo.php");
-                    document.getElementById("disp").innerHTML = partOne  + "      " + result;
+                    var total = partOne  + "      " + result;
+                    document.getElementById("disp").innerHTML = total.fontcolor("red")
                 
             } 
             else if(document.getElementById('w2').checked) { 
                var partOne = "Almost! Not quite correct, remember spelling is a key part in programming.";  
                var str = "Next Question";
                     var result = str.link("partTwoQuestionTwo.php");
-                    document.getElementById("disp").innerHTML = partOne  + "      " + result; 
+                    var total = partOne  + "      " + result;
+                    document.getElementById("disp").innerHTML = total.fontcolor("red")
             } 
             else if(document.getElementById('c1').checked) { 
                 var partOne = "This is correct! ";   
                 var str = "Next Question";
                     var result = str.link("partTwoQuestionTwo.php");
-                    document.getElementById("disp").innerHTML = partOne  + "      " + result;
+                    var total = partOne  + "      " + result;
+                    document.getElementById("disp").innerHTML = total.fontcolor("green")
             } 
             else { 
                 var partOne = "The task was to store yellow in the variable colour, so the best way to do that would be to select the second option - colour = 'yellow'"; 
